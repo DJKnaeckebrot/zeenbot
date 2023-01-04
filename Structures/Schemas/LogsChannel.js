0679a3b21e8d5d34a7c7b811f44dac19f309b8ef
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2023-01-04 20:04:59 +0100

    Feature: Added IgnoredChannels and AntiCrash

--- Structures/Schemas/LogsChannel.js
+++ Structures/Schemas/LogsChannel.js
@@ -3,5 +3,6 @@ const { model, Schema } = require("mongoose")
 module.exports = model("loggerChannel", new Schema({
     Guild: String,
     Channel: String,
-    IgnoreChannels: Array
+    IgnoreChannels: Array,
+    AntiCrashChannel: String,
 }))
