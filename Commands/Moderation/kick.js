06d2135c7c94d163358d5c2edf3e5ff6f329a84d
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2023-01-07 18:55:03 +0100

    Feature: Add Kick command

--- Commands/Moderation/kick.js
+++ Commands/Moderation/kick.js
@@ -80,7 +80,7 @@ module.exports = {
 
                 case "kick-yes": {
 
-                    // member.kick({ reason })
+                    member.kick({ reason })
 
                     interaction.editReply({
                         embeds: [
