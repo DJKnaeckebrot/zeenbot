53d3be8f10f0b42f01213bd423e57594f70b5965
mrdennis1212 <46864390+mrdennis1212@users.noreply.github.com>
2023-01-09 11:21:26 +0100

    Update Schema for RR

--- Structures/Schemas/ReactionRoles.js
+++ Structures/Schemas/ReactionRoles.js
@@ -3,6 +3,10 @@ const { model, Schema } = require("mongoose")
 module.exports = model("reactionroles", new Schema({
 
     GuildID: String,
-    roles: Array
+    roles: Array,
+    Status: { type: Boolean, default: false },
+    ChannelID: String,
+    MessageID: String,
+    Text: { type: String, default: "Please select a role below" },
 
 }))
